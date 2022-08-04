9225a20d85487984cf9af1b93f8dcf4a628fa2e7
Artem Baykov <baikoff@bk.ru>
2022-08-04 17:42:32 +0300

    Спринг дата в роле

--- src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDAO.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDAO.java
@@ -1,12 +1,10 @@
 package ru.kata.spring.boot_security.demo.dao;
 
-import ru.kata.spring.boot_security.demo.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.kata.spring.boot_security.demo.model.User;
 
-import java.util.List;
+@Repository
+public interface RoleDAO extends JpaRepository<User, Long> {
 
-public interface RoleDAO {
-
-    List<Role> allRoles();
-
-    Role findRoleById(Long id);
 }
